9d3d26469217b1dfe41009cfe46ea533eb4cf598
Dave Roberge <david.roberge@maine.edu>
2012-06-12 13:05:49 -0400

    Added missing functionality for deleting residencies

--- controller/src/main/java/org/openhds/controller/service/ResidencyService.java
+++ controller/src/main/java/org/openhds/controller/service/ResidencyService.java
@@ -12,58 +12,85 @@ import org.openhds.domain.model.PrivilegeConstants;
 import org.openhds.domain.model.Residency;
 
 /**
- * A service class to be used when dealing with the Residency class
- * This service provides various methods to verify the integrity of a Residency in the system
+ * A service class to be used when dealing with the Residency class This service
+ * provides various methods to verify the integrity of a Residency in the system
  * 
  * @author Dave
- *
+ * 
  */
 public interface ResidencyService {
 
 	/**
-	 * Determine if the a residency is valid for an Individual.
-	 * Specifically, look at fields related to starting a residency (start date, start type, etc.)
-	 * and verify they are valid compared to previous residencies for the Individual
+	 * Determine if the a residency is valid for an Individual. Specifically,
+	 * look at fields related to starting a residency (start date, start type,
+	 * etc.) and verify they are valid compared to previous residencies for the
+	 * Individual
 	 * 
-	 * @param residency the residency to evaluate
+	 * @param residency
+	 *            the residency to evaluate
 	 * @return the same residency object passed in as an argument
-	 * @throws ConstraintViolations if any integrity constraints are violated
+	 * @throws ConstraintViolations
+	 *             if any integrity constraints are violated
 	 */
-	@Authorized({PrivilegeConstants.CREATE_ENTITY})
-	Residency evaluateResidency(Residency residency) throws ConstraintViolations;
-	
+	@Authorized({ PrivilegeConstants.CREATE_ENTITY })
+	Residency evaluateResidency(Residency residency)
+			throws ConstraintViolations;
+
 	/**
 	 * Create a residency.
 	 * 
-	 * @param individual the individual for the residency
-	 * @param location the location of the residency
-	 * @param startDate the start date of the residency
-	 * @param startType the start type of the residency
-	 * @param collectedBy The field worker who collected the residency information
+	 * @param individual
+	 *            the individual for the residency
+	 * @param location
+	 *            the location of the residency
+	 * @param startDate
+	 *            the start date of the residency
+	 * @param startType
+	 *            the start type of the residency
+	 * @param collectedBy
+	 *            The field worker who collected the residency information
 	 * @return A newly created residency
 	 */
-	@Authorized({PrivilegeConstants.CREATE_ENTITY})
-	Residency createResidency(Individual individual, Location location, Calendar startDate, String startType, FieldWorker collectedBy);
-	
+	@Authorized({ PrivilegeConstants.CREATE_ENTITY })
+	Residency createResidency(Individual individual, Location location,
+			Calendar startDate, String startType, FieldWorker collectedBy);
+
 	/**
-	 * Retrieve a list of individual who have their last known residency (current residency) for a given location
+	 * Retrieve a list of individual who have their last known residency
+	 * (current residency) for a given location
 	 * 
-	 * @param location The location to look for residencies at
-	 * @return a list of individuals who have a current residency at the location
+	 * @param location
+	 *            The location to look for residencies at
+	 * @return a list of individuals who have a current residency at the
+	 *         location
 	 */
-	@Authorized({PrivilegeConstants.VIEW_ENTITY, PrivilegeConstants.ACCESS_UPDATE, PrivilegeConstants.ACCESS_BASELINE})
+	@Authorized({ PrivilegeConstants.VIEW_ENTITY,
+			PrivilegeConstants.ACCESS_UPDATE,
+			PrivilegeConstants.ACCESS_BASELINE })
 	List<Individual> getIndividualsByLocation(Location location);
-	
+
 	/**
-	 * Determine if an Individual has a current open residency. An open residency is defined as a residency
-	 * that has no end date
+	 * Determine if an Individual has a current open residency. An open
+	 * residency is defined as a residency that has no end date
 	 * 
-	 * @param individual the individual to check for an open residency
-	 * @return true if the individual has atleast 1 open residency, false otherwise
+	 * @param individual
+	 *            the individual to check for an open residency
+	 * @return true if the individual has atleast 1 open residency, false
+	 *         otherwise
 	 */
-	@Authorized({PrivilegeConstants.CREATE_ENTITY})
+	@Authorized({ PrivilegeConstants.CREATE_ENTITY })
 	boolean hasOpenResidency(Individual individual);
-	
-	@Authorized({PrivilegeConstants.VIEW_ENTITY})
-    List<Residency> getAllResidencies(Individual individual);
+
+	@Authorized({ PrivilegeConstants.VIEW_ENTITY })
+	List<Residency> getAllResidencies(Individual individual);
+
+	/**
+	 * Return the associated events for a given residency (e.g. Pregnancy
+	 * outcome, in migration, out migration, death).
+	 * 
+	 * @return the associated events for this residency
+	 */
+
+	List<AuditableCollectedEntity> getResidencyAssociatedEvents(
+			Residency residency);
 }
--- web/src/main/java/org/openhds/web/crud/impl/ResidencyCrudImpl.java
+++ web/src/main/java/org/openhds/web/crud/impl/ResidencyCrudImpl.java
@@ -3,13 +3,26 @@ package org.openhds.web.crud.impl;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+
 import org.openhds.controller.exception.ConstraintViolations;
-import org.openhds.domain.model.Residency;
 import org.openhds.controller.service.ResidencyService;
+import org.openhds.domain.model.AuditableCollectedEntity;
+import org.openhds.domain.model.Death;
+import org.openhds.domain.model.InMigration;
+import org.openhds.domain.model.OutMigration;
+import org.openhds.domain.model.PregnancyOutcome;
+import org.openhds.domain.model.Residency;
 
 public class ResidencyCrudImpl extends EntityCrudImpl<Residency, String> {
-	
+
 	private ResidencyService residencyService;
+
+	// a list of associated events for a residency.
+	// only used when attempting to delete a residency
+	private List<AuditableCollectedEntity> events;
 
 	public ResidencyCrudImpl(Class<Residency> entityClass) {
 		super(entityClass);
@@ -24,17 +37,17 @@ public class ResidencyCrudImpl extends EntityCrudImpl<Residency, String> {
 		} catch (ConstraintViolations e) {
 			jsfService.addError(e.getMessage());
 		}
-		
+
 		return null;
 	}
-	
-    public Date getStartDate() {
-    	
-    	if (entityItem.getStartDate() == null) {
-    		return new Date();
-    	}
-    	
-    	return entityItem.getStartDate().getTime();
+
+	public Date getStartDate() {
+
+		if (entityItem.getStartDate() == null) {
+			return new Date();
+		}
+
+		return entityItem.getStartDate().getTime();
 	}
 
 	public void setStartDate(Date startDate) throws ParseException {
@@ -49,5 +62,69 @@ public class ResidencyCrudImpl extends EntityCrudImpl<Residency, String> {
 
 	public void setResidencyService(ResidencyService residencyService) {
 		this.residencyService = residencyService;
+	}
+
+	@Override
+	public String delete() {
+		Residency persistentObject = (Residency) converter.getAsObject(
+				FacesContext.getCurrentInstance(), null,
+				jsfService.getReqParam("itemId"));
+		List<AuditableCollectedEntity> entities = residencyService
+				.getResidencyAssociatedEvents(persistentObject);
+		if (entities.size() > 0) {
+			events = entities;
+			showListing = false;
+			return "residency_delete_failure";
+		}
+
+		return super.delete();
+	}
+
+	public String getStartEventName() {
+		for (AuditableCollectedEntity en : events) {
+			if (en instanceof InMigration) {
+				return "In Migration";
+			}
+
+			if (en instanceof PregnancyOutcome) {
+				return "Birth";
+			}
+		}
+
+		return "";
+	}
+
+	public String getStartEventUuid() {
+		for (AuditableCollectedEntity en : events) {
+			if (en instanceof InMigration || en instanceof PregnancyOutcome) {
+				return en.getUuid();
+			}
+		}
+
+		return "";
+	}
+
+	public String getEndEventName() {
+		for (AuditableCollectedEntity en : events) {
+			if (en instanceof Death) {
+				return "Death";
+			}
+
+			if (en instanceof OutMigration) {
+				return "Out Migration";
+			}
+		}
+
+		return "";
+	}
+
+	public String getEndEventUuid() {
+		for (AuditableCollectedEntity en : events) {
+			if (en instanceof Death || en instanceof OutMigration) {
+				return en.getUuid();
+			}
+		}
+
+		return "";
 	}
 }
