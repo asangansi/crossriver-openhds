c5d3979f2ac7209d25a94d65c7e34752a20ba2b9
Dave Roberge <david.roberge@maine.edu>
2012-09-11 15:05:54 -0400

    Revert "Started refactoring"
    This reverts commit 1316486f9e1be30f8c27678f5d8910ec12a836a0.

--- controller/src/main/java/org/openhds/controller/service/ResidencyService.java
+++ controller/src/main/java/org/openhds/controller/service/ResidencyService.java
@@ -55,6 +55,16 @@ public interface ResidencyService {
             FieldWorker collectedBy);
 
     /**
+     * Retrieve a list of individual who have their last known residency (current residency) for a given location
+     * 
+     * @param location
+     *            The location to look for residencies at
+     * @return a list of individuals who have a current residency at the location
+     */
+    @Authorized({ PrivilegeConstants.VIEW_ENTITY, PrivilegeConstants.ACCESS_UPDATE, PrivilegeConstants.ACCESS_BASELINE })
+    List<Individual> getIndividualsByLocation(Location location, boolean includeEndedResidency);
+
+    /**
      * Determine if an Individual has a current open residency. An open residency is defined as a residency that has no
      * end date
      * 
@@ -65,4 +75,14 @@ public interface ResidencyService {
     @Authorized({ PrivilegeConstants.CREATE_ENTITY })
     boolean hasOpenResidency(Individual individual);
 
+    @Authorized({ PrivilegeConstants.VIEW_ENTITY })
+    List<Residency> getAllResidencies(Individual individual);
+
+    /**
+     * Return the associated events for a given residency (e.g. Pregnancy outcome, in migration, out migration, death).
+     * 
+     * @return the associated events for this residency
+     */
+
+    List<AuditableCollectedEntity> getResidencyAssociatedEvents(Residency residency);
 }
--- controller/src/test/java/org/openhds/controller/service/impl/ResidencyServiceImplTest.java
+++
@@ -1,5 +0,0 @@
-package org.openhds.controller.service.impl;
-
-public class ResidencyServiceImplTest {
-
-}
